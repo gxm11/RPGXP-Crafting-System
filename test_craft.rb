679df5e4d88eb6f9e137ee978a609e8dc7fc10e2
guoxm <xiaomi.guo@outlook.com>
2024-07-28 14:58:17 +0800

    Added error log and updated crafting recipes in test_craft.rb.

--- test_craft.rb
+++ test_craft.rb
@@ -8,25 +8,30 @@ load "window_craftnumber.rb"
 load "window_craftstatus.rb"
 
 $game_craft = Game_Craft.new
+
 $game_craft.add_recipe do |craft|
-  craft.create :item, 1, 2
-  craft.consume :item, 2, 10
-  craft.consume :item, 3, 5
+  craft.create :item, 1, 3
+  craft.consume :item, 2, 1
+end
+$game_craft.add_recipe do |craft|
+  craft.create :item, 2, 1
+  craft.consume :item, 1, 4
 end
 $game_craft.add_recipe do |craft|
   craft.create :weapon, 2, 1
-  craft.consume :item, 2, 20
-  craft.consume :weapon, 3, 10
-end
-$game_craft.add_recipe do |craft|
-  craft.create :armor, 2, 1
-  craft.consume :item, 2, 20
-  craft.consume :armor, 3, 10
+  craft.consume :weapon, 1, 2
 end
 
 class Interpreter
   def init_test
     return unless $DEBUG
+    # 添加一些物品和装备，用于合成测试。
+    $game_party.gain_item(1, 10)
+    $game_party.gain_item(2, 10)
+    $game_party.gain_weapon(1, 10)
+    $game_party.gain_weapon(2, 10)
+    $game_party.gain_armor(1, 10)
+    $game_party.gain_armor(2, 10)
 
     # 进入合成场景
     $scene = Scene_Craft.new
